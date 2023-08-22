100fc703a64734f801d3a792a123d8437173c171
Tyrese <tyresejin3915@gmail.com>
2023-08-22 16:40:59 +0900

    fix: style

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -17,45 +17,45 @@ const Component = () => {
       <div className="float-left">
         <Icon
           icon="ri:home-line"
-          className="w-[32px] h-[32px] m-2 text-white"
+          className="w-[32px] h-[32px] m-[8px] text-white"
         />
       </div>
       <div>
-        <button className="h-[48px] py-0 px-4 my-0 mx-3">button2</button>
-        <button className="h-[48px] py-0 px-4 my-0 mx-3 bg-[#241F2E]">
+        <button className="h-[48px] py-0 px-[16px] m-0">button2</button>
+        <button className="h-[48px] py-0 px-[16px] m-0 bg-[#241F2E]">
           button3
         </button>
       </div>
       <div className="float-right flex">
-        <button className="h-[32px] py-1 px-4 my-2 mx-3 bg-[#24281C]">
+        <button className="h-[32px] py-[4px] px-[16px] my-[8px] mx-[12px] bg-[#24281C]">
           button1
         </button>
         <Icon
           icon="mingcute:user-4-fill"
-          className="w-[24px] h-[24px] m-3 text-[#494D41]"
+          className="w-[24px] h-[24px] m-[12px] text-[#494D41]"
         />
         <Icon
           icon="icon-park-twotone:handle-right"
-          className="w-[24px] h-[24px] m-3 text-[#494D41]"
+          className="w-[24px] h-[24px] m-[12px] text-[#494D41]"
         />
-        <button className="h-[32px] py-1 px-4 my-2 mx-3 bg-[#F0D244] text-black">
+        <button className="h-[32px] py-[4px] px-[16px] my-[8px] mx-[12px] bg-[#F0D244] text-black">
           button1
         </button>
         <Icon
           icon="material-symbols:menu"
-          className="w-[24px] h-[24px] m-3 text-[#494D41]"
+          className="w-[24px] h-[24px] m-[12px] text-[#494D41]"
         />
         <Icon
           icon="fe:minus"
-          className="w-[24px] h-[24px] m-3 text-[#494D41]"
+          className="w-[24px] h-[24px] m-[12px] text-[#494D41]"
         />
         <Icon
           icon="codicon:chrome-maximize"
-          className="w-[24px] h-[24px] m-3 text-[#494D41]"
+          className="w-[24px] h-[24px] m-[12px] text-[#494D41]"
         />
         <Icon
           icon="material-symbols:close"
-          className="w-[24px] h-[24px] m-3 text-[#494D41]"
+          className="w-[24px] h-[24px] m-[12px] text-[#494D41]"
         />
       </div>
     </div>
