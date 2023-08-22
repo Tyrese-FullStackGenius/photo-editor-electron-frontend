95ed042e9c673003e83efa1882dd8b6b7b03c133
terriellis120 <terriellis120@gmail.com>
2023-08-23 06:15:29 +0900

    feat: modals

--- src/components/home/projects.js
+++ src/components/home/projects.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ProjectCard from "./projectcard";
 import RecycleCard from "./recyclecard";
 import { Icon } from "@iconify/react";
-import { OverlayTrigger, Tooltip } from "react-bootstrap";
+import { OverlayTrigger, Tooltip, Modal } from "react-bootstrap";
 
 function Projects(props) {
   const [activeTab, setActiveTab] = useState("all");
@@ -10,6 +10,18 @@ function Projects(props) {
   const renderTooltip = (tooltipText) => (
     <Tooltip id="tooltip">{tooltipText}</Tooltip>
   );
+
+  const [showNewProjectModal, setShowNewProjectModal] = useState(false);
+
+  const handleNewProject = () => {
+    setShowNewProjectModal(false);
+  };
+
+  const [showClearTrashModal, setShowClearTrashModal] = useState(false);
+
+  const handleClearTrash = () => {
+    setShowClearTrashModal(false);
+  };
 
   const trashdata = [
     {
@@ -72,7 +84,10 @@ function Projects(props) {
         }`}
       >
         <p className="my-auto">共{trashdata.length}个项目</p>
-        <button className="text-white bg-[#2b2f2f] px-[10px] py-[5px] hover:opacity-90 active:opacity-50 ">
+        <button
+          className="text-white bg-[#2b2f2f] px-[10px] py-[5px] hover:opacity-90 active:opacity-50 "
+          onClick={() => setShowClearTrashModal(true)}
+        >
           清空回收站
         </button>
       </div>
@@ -81,7 +96,12 @@ function Projects(props) {
           activeTab === "all" ? "block" : "hidden"
         }`}
       >
-        <button className="w-[250px] h-[250px] hover:opacity-60 flex rounded-[5px] border-2">
+        <button
+          className="w-[250px] h-[250px] hover:opacity-60 flex rounded-[5px] border-2"
+          onClick={() => {
+            setShowNewProjectModal(true);
+          }}
+        >
           <p className="text-[128px] my-auto mx-auto"> + </p>
         </button>
         {projectsdata.map((item, index) => (
@@ -92,6 +112,30 @@ function Projects(props) {
           />
         ))}
       </div>
+      <Modal
+        show={showNewProjectModal}
+        onHide={() => {
+          setShowNewProjectModal(false);
+        }}
+        centered
+      >
+        <div className="bg-[#000401] text-white">
+          <div className="flex my-[15px]">
+            <p className=" text-[25px] py-[10px] mx-auto ">创建新项目</p>
+          </div>
+          <div className="flex my-[15px]">
+            <input className="text-[15px] py-[5px] px-[20px] mx-auto bg-[#2f242f] text-white w-[80%]"></input>
+          </div>
+          <div className="flex my-[15px]">
+            <button
+              className="text-black text-[20px] w-[60%] bg-[#ffff00] mx-auto opacity-90 hover:opacity-100 active:opacity-60"
+              onClick={handleNewProject}
+            >
+              保存
+            </button>
+          </div>
+        </div>
+      </Modal>
       <div
         className={`mt-[90px] grid grid-cols-3 gap-y-[30px] ${
           activeTab === "all" ? "hidden" : "block"
@@ -105,6 +149,41 @@ function Projects(props) {
           />
         ))}
       </div>
+      <Modal
+        show={showClearTrashModal}
+        onHide={() => {
+          setShowClearTrashModal(false);
+        }}
+        centered
+      >
+        <div className="bg-[#000401] text-white">
+          <div className="flex my-[15px]">
+            <p className=" text-[25px] py-[10px] mx-auto ">是否清空回收站</p>
+          </div>
+          <div className="flex my-[15px]">
+            <p className=" text-[20px] py-[10px] px-[30px] text-center mx-auto">
+              清空回收站后将切蒂删除回收站内所有内容且不可还原，是否继续？
+            </p>
+          </div>
+
+          <div className="flex my-[15px]">
+            <button
+              className="text-black text-[20px] w-[40%] bg-[#ffffff] mx-auto opacity-90 hover:opacity-100 active:opacity-60"
+              onClick={() => {
+                setShowClearTrashModal(false);
+              }}
+            >
+              取消
+            </button>
+            <button
+              className="text-black text-[20px] w-[40%] bg-[#ffff00] mx-auto opacity-90 hover:opacity-100 active:opacity-60"
+              onClick={handleClearTrash}
+            >
+              确认
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
